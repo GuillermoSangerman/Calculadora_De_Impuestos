45a29db0005a2b8e67128d1615c86c80ca90e1b0
Guillermo Sangerman <sangermanmptn8@gmail.com>
2024-12-06 14:47:32 -0600

    agregando condiciones

--- script/main.js
+++ script/main.js
@@ -10,21 +10,35 @@ const resultsContainerCal = document.querySelector('.results_container_cal')
 const resultsRepayments = document.querySelector('#results_repayments')
 const resultsRepay = document.querySelector('#results_repay')
 
-mountgageAmount.value = 0
-mountgageTerm.value = 0
-interestRate.value = 0
+mountgageAmount.value = ""
+mountgageTerm.value = ""
+interestRate.value = ""
 resultsRepay.value = 0
 resultsRepayments.value = 0
 let p = 0
 let r = 0
 let n = 0
 
+function cliqueo() {
+
+}
 function agregarInput() {
-    p = parseFloat(mountgageAmount.value)
-    r = obtenerIntereses(parseFloat(interestRate.value))
-    n = obtenerMeses(parseFloat(mountgageTerm.value))
-    console.log(p, r, n);
-    calculoDeIntereses(p, r, n)
+    if (mountgageAmount.value === "" ||
+        mountgageTerm.value === "" ||
+        interestRate.value === "") {
+        console.log("casilla vacia");
+    } else {
+        resultsContainer.classList.add('d-none')
+        resultsContainerCal.classList.remove('d-none')
+        p = parseFloat(mountgageAmount.value)
+        r = obtenerIntereses(parseFloat(interestRate.value))
+        n = obtenerMeses(parseFloat(mountgageTerm.value))
+        console.log(p, r, n);
+        soloIntereses(p, r, n)
+    }
+
+
+
 }
 
 function obtenerIntereses(r) {
@@ -37,22 +51,28 @@ function obtenerMeses(n) {
 }
 function calculoDeIntereses(p, r, n) {
     let primerValor = p * r * ((1 + r) ** n)
-    console.log(primerValor);
     let segundoValor = ((1 + r) ** n) - 1
-    console.log(segundoValor);
     let res = (primerValor / segundoValor)
-    console.log(res);
     resultsRepayments.value = res.toFixed(2)
     resultsRepay.value = (res * n).toFixed(2)
 }
-// //  M = Pago mensual.
+function soloIntereses(p, r, n) {
+    let primerValor = p * r * ((1 + r) ** n)
+    let segundoValor = ((1 + r) ** n) - 1
+    let res = (primerValor / segundoValor)
+    let multiplicoYResto = (res * n) - p
+    let divido = multiplicoYResto / n
+    resultsRepayments.value = divido.toFixed(2)
+    resultsRepay.value = multiplicoYResto.toFixed(2)
+}
+//  M = Pago mensual.
 //  P = Monto del préstamo.
 //  r = Tasa de interés mensual (tasa de interés anual dividida por 12).
 //  n = Número total de pagos (número de años multiplicado por 12).
 function resetAll() {
-    mountgageAmount.value = 0
-    mountgageTerm.value = 0
-    interestRate.value = 0
+    mountgageAmount.value = ""
+    mountgageTerm.value = ""
+    interestRate.value = ""
     resultsRepay.value = 0
     resultsRepayments.value = 0
     //location.reload()
@@ -64,7 +84,5 @@ clearAll.addEventListener('click', () => {
 })
 
 btnCalculate.addEventListener('click', () => {
-    resultsContainer.classList.add('d-none')
-    resultsContainerCal.classList.remove('d-none')
     agregarInput()
 })
