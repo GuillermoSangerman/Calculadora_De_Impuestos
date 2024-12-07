d2be520e09096069f9eef18c482dc6ffda63afd2
Guillermo Sangerman <sangermanmptn8@gmail.com>
2024-12-07 01:31:02 -0600

    agregando condicione de invalid

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 const mountgageAmount = document.querySelector('#mountgage_amount')
+const numberIcon = document.querySelector('#number_icon')
 const mountgageTerm = document.querySelector('#mountgage_term')
 const interestRate = document.querySelector('#interest_rate')
 const repayment = document.querySelector('#repayment')
@@ -9,6 +10,13 @@ const resultsContainer = document.querySelector('.results_container')
 const resultsContainerCal = document.querySelector('.results_container_cal')
 const resultsRepayments = document.querySelector('#results_repayments')
 const resultsRepay = document.querySelector('#results_repay')
+const container = document.querySelector('.container_desktop')
+const mountgageAmountInvalid = document.querySelector("#mountgage_amount_invalid")
+const mountgageTermIvalid = document.querySelector("#mountgage_term_invalid")
+const interestRateInvalid = document.querySelector("#interest_rate_invalid")
+const repaymentInvalid = document.querySelector('#repayment_invalid')
+const numberIconYears = document.querySelector('#number_icon_years')
+const numberIconPorcentaje = document.querySelector('#number_icon_porcentaje')
 
 mountgageAmount.value = ""
 mountgageTerm.value = ""
@@ -19,26 +27,123 @@ let p = 0
 let r = 0
 let n = 0
 
-function cliqueo() {
-
+function repaymentChecked() {
+    resultsContainer.classList.add('d-none')
+    resultsContainerCal.classList.remove('d-none')
+    p = parseFloat(mountgageAmount.value)
+    r = obtenerIntereses(parseFloat(interestRate.value))
+    n = obtenerMeses(parseFloat(mountgageTerm.value))
+    console.log(p, r, n);
+    calculoDeIntereses(p, r, n)
+}
+function interestOnlyChecked() {
+    resultsContainer.classList.add('d-none')
+    resultsContainerCal.classList.remove('d-none')
+    p = parseFloat(mountgageAmount.value)
+    r = obtenerIntereses(parseFloat(interestRate.value))
+    n = obtenerMeses(parseFloat(mountgageTerm.value))
+    console.log(p, r, n);
+    soloIntereses(p, r, n)
 }
 function agregarInput() {
-    if (mountgageAmount.value === "" ||
-        mountgageTerm.value === "" ||
-        interestRate.value === "") {
-        console.log("casilla vacia");
-    } else {
-        resultsContainer.classList.add('d-none')
-        resultsContainerCal.classList.remove('d-none')
-        p = parseFloat(mountgageAmount.value)
-        r = obtenerIntereses(parseFloat(interestRate.value))
-        n = obtenerMeses(parseFloat(mountgageTerm.value))
-        console.log(p, r, n);
-        soloIntereses(p, r, n)
+    console.log(repayment.checked, interestOnly.checked);
+    if (mountgageAmount.value === "" || mountgageAmount.value < 0 ||
+        mountgageTerm.value === "" || mountgageTerm.value < 0 ||
+        interestRate.value === "" || interestRate.value < 0 ||
+        repayment.checked === false || interestOnly.checked === false
+    ) {
+        if (mountgageAmount.value === "" || mountgageAmount.value < 0) {
+            mountgageAmountInvalid.classList.add('d-block')
+            mountgageAmount.classList.add(`mountgage_invalid`)
+            mountgageAmount.classList.add(`focus_invalid`)
+            mountgageAmount.classList.remove('focus_content')
+            mountgageAmount.classList.remove('mountgage_amount')
+            numberIcon.classList.add('number_invalid')
+            numberIcon.classList.remove('number_icon')
+        } else {
+            mountgageAmountInvalid.classList.add('d-none')
+            mountgageAmount.classList.remove(`mountgage_invalid`)
+            mountgageAmount.classList.remove(`focus_invalid`)
+            mountgageAmount.classList.add('focus_content')
+            mountgageAmount.classList.add('mountgage_amount')
+            numberIcon.classList.remove('number_invalid')
+            numberIcon.classList.add('number_icon')
+        }
+        if (mountgageTerm.value === "" || mountgageTerm.value < 0) {
+            mountgageTermIvalid.classList.add('d-block')
+            mountgageTerm.classList.add(`mountgage_invalid`)
+            mountgageTerm.classList.add(`focus_invalid`)
+            mountgageTerm.classList.remove('focus_content')
+            mountgageTerm.classList.remove('mountgage_term')
+            numberIconYears.classList.add('number_invalid')
+            numberIconYears.classList.remove('number_icon_years')
+        } else {
+            mountgageTermIvalid.classList.add('d-none')
+            mountgageTerm.classList.remove(`mountgage_invalid`)
+            mountgageTerm.classList.remove(`focus_invalid`)
+            mountgageTerm.classList.add('focus_content')
+            mountgageTerm.classList.add('mountgage_term')
+            numberIconYears.classList.remove('number_invalid')
+            numberIconYears.classList.add('number_icon_years')
+        }
+        if (interestRate.value === "" || interestRate.value < 0) {
+            interestRateInvalid.classList.add('d-block')
+            interestRate.classList.add(`mountgage_invalid`)
+            interestRate.classList.add(`focus_invalid`)
+            interestRate.classList.remove('focus_content')
+            interestRate.classList.remove('mountgage_term')
+            numberIconPorcentaje.classList.add('number_invalid')
+            numberIconPorcentaje.classList.remove('number_icon_years')
+        } else {
+            interestRateInvalid.classList.add('d-none')
+            interestRate.classList.remove(`mountgage_invalid`)
+            interestRate.classList.remove(`focus_invalid`)
+            interestRate.classList.add('focus_content')
+            interestRate.classList.add('mountgage_term')
+            numberIconPorcentaje.classList.remove('number_invalid')
+            numberIconPorcentaje.classList.add('number_icon_years')
+        }
+        if (repayment.checked === false || interestOnly.checked === false) {
+            repaymentInvalid.classList.add("d-block")
+        } else if (repayment.checked || interestOnly.checked) {
+            repaymentInvalid.classList.add("d-none")
+            repaymentInvalid.classList.remove("d-block")
+        }
     }
+    else {
+        if (repayment.checked) {
+            repaymentChecked()
 
 
 
+
+
+        } else if (interestOnly.checked) {
+            interestOnlyChecked()
+            // mountgageAmountInvalid.classList.add('d-none')
+            // mountgageAmount.classList.remove(`mountgage_invalid`)
+            // mountgageAmount.classList.remove(`focus_invalid`)
+            // mountgageAmount.classList.add('focus_content')
+            // mountgageAmount.classList.add('mountgage_amount')
+            // numberIcon.classList.remove('number_invalid')
+            // numberIcon.classList.add('number_icon')
+            // mountgageTermIvalid.classList.add('d-none')
+            // mountgageTerm.classList.remove(`mountgage_invalid`)
+            // mountgageTerm.classList.remove(`focus_invalid`)
+            // mountgageTerm.classList.add('focus_content')
+            // mountgageTerm.classList.add('mountgage_term')
+            // numberIconYears.classList.remove('number_invalid')
+            // numberIconYears.classList.add('number_icon_years')
+            // interestRateInvalid.classList.add('d-none')
+            // interestRate.classList.remove(`mountgage_invalid`)
+            // interestRate.classList.remove(`focus_invalid`)
+            // interestRate.classList.add('focus_content')
+            // interestRate.classList.add('mountgage_term')
+            // numberIconPorcentaje.classList.remove('number_invalid')
+            // numberIconPorcentaje.classList.add('number_icon_years')
+            // repaymentInvalid.classList.add("d-none")
+        }
+    }
 }
 
 function obtenerIntereses(r) {
@@ -83,6 +188,4 @@ clearAll.addEventListener('click', () => {
     resetAll()
 })
 
-btnCalculate.addEventListener('click', () => {
-    agregarInput()
-})
+btnCalculate.addEventListener('click', agregarInput)
